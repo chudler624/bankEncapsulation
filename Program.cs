f2671f739577d0321c3ff450b3efe91c3e2c2772
Casey Hudler <chudler624@gmail.com>
2022-08-19 12:16:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BankEncapsulation;
+
+BankAccount myAccount = new BankAccount();
+
+
+
+Console.WriteLine("How much do you want to deposit?");
+double depositAmount = double.Parse(Console.ReadLine());
+
+
+myAccount.Deposit(depositAmount);
+
+
+Console.WriteLine($"My new balance amount is {myAccount.GetBalance()}");
